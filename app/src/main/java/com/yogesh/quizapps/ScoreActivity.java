3689010b17feb3751f286c03951473366f04d2bb
yogesrai <yogesrai100@gmail.com>
2020-03-21 15:14:12 +0545

    Quiz app

--- app/src/main/java/com/yogesh/quizapps/ScoreActivity.java
+++ app/src/main/java/com/yogesh/quizapps/ScoreActivity.java
@@ -2,13 +2,21 @@ package com.yogesh.quizapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+        textView = findViewById(R.id.textView);
+
+        Intent intent = getIntent();
+        String score = intent.getStringExtra("SCORE");
+        textView.setText(score);
     }
 }
